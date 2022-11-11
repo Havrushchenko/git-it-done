be6d956b441567dfb5580140ee1c6b837ed8b567
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-11-11 17:17:49 +0200

    handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,24 +1,45 @@
+var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
-var getRepoIssues = function (repo) {
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
+
+  var getRepoIssues = function(repo) {
+    // format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function (response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function (data) {
-                // pass response data to dom function
-                displayIssues(data);
-                // check if api has paginated issues
-                if (response.headers.get("Link")) {
-                    console.log(displayWarning(repo));
-                  }
-            });
-        }
-        else {
-            alert("There was a problem with your request!");
-        }
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+  
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            displayWarning(repo);
+          }
+        });
+      } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
+      }
     });
-};
+  };
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
